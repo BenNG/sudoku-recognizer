1f3bfecb7019f395b804ce95c8511343c65ce98f
BenNG <benjamin.kim.nguyen@gmail.com>
2016-09-14 19:55:49 +0200

    drawing a border around the sudoku (WIP)

--- main.cpp
+++ main.cpp
@@ -2,31 +2,60 @@
 #include <opencv2/opencv.hpp>
 
 using namespace cv;
+using namespace std;
 /*
  * preprocess
  *
  * It's going to be easier for the opencv algorithm to detect the border of the sudoku
+ * There is a Threshold where everything is white or black
  */
 Mat preprocess(Mat input){
     Mat outerBox = Mat(input.size(), CV_8UC1);
     GaussianBlur(input, input, Size(11, 11), 0);
     adaptiveThreshold(input, outerBox, 255, ADAPTIVE_THRESH_MEAN_C, THRESH_BINARY, 5, 2);
     bitwise_not(outerBox, outerBox);
-    Mat kernel = (Mat_<uchar>(3,3) << 0,1,0,1,1,1,0,1,0);
-    dilate(outerBox, outerBox, kernel);
+    dilate(outerBox, outerBox, Mat());
     return outerBox;
 }
 
+/*
+ * Find the biggest contour in the image
+ * note that it returns vector< vector<Point> > because it is more convenient to use drawContours after
+ * */
+vector< vector<Point> > findBigestContour(Mat input){
+
+    int largest_area=0;
+    int largest_contour_index=0;
+    vector<Point> contour;
+    vector< vector<Point> > contours;
+
+    findContours(input, contours, RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
+
+    for( int i = 0; i< contours.size(); i++ ) {
+        contour = contours[i];
+            double a = contourArea(contour);
+            if(a > largest_area){
+                largest_area = a;
+                largest_contour_index = i;
+            }
+    }
+    contour = contours[largest_contour_index];
+    cout << contour << endl;
+    std::vector< vector<Point> > biggest (1,contour);
+    return biggest;
+}
+
 int main(int argc, char **argv) {
+    Mat sudoku = imread("puzzles/sudoku3.jpg", CV_LOAD_IMAGE_GRAYSCALE);
 
-    Mat sudoku = imread("puzzles/sudoku.jpg", 0);
-    Mat original = imread("puzzles/sudoku.jpg", 0);
+    Mat preprocessed = preprocess(sudoku.clone());
+    vector< vector<Point> > biggestContour = findBigestContour(preprocessed);
 
-    preprocess(sudoku);
+    Scalar color( 255,255,255);
+    drawContours( sudoku, biggestContour, 0, color, 2, 8);
 
     namedWindow("Display Image", WINDOW_AUTOSIZE);
     imshow("Display Image", sudoku);
     waitKey(0);
     return 0;
-
 }
