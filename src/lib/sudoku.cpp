7a65b0b914e47c60437d399bf4832e4bb10adfad
BenNG <benjamin.kim.nguyen@gmail.com>
2017-01-26 10:48:09 +0100

    refact askServerForSolution

--- src/lib/sudoku.cpp
+++ src/lib/sudoku.cpp
@@ -1790,73 +1790,86 @@ WriteMemoryCallback(void *contents, size_t size, size_t nmemb, void *userp)
 string askServerForSolution(string initialStateOfTheSudoku)
 {
 
-    // request the server for solution --------------------------------
 
-    stringstream ss, url;
+    // string solution = askServerForSolution(initialStateOfTheSudoku);
+    Sudoku::init();
 
-    url << "http://moulinet.tech/sudokus?input=";
-    url << initialStateOfTheSudoku;
+    auto S = solve(unique_ptr<Sudoku>(new Sudoku(initialStateOfTheSudoku)));
+    string solution = S->getSolution();
 
-    CURL *curl_handle;
-    CURLcode res;
-
-    struct MemoryStruct chunk;
-
-    chunk.memory = (char *)malloc(1); /* will be grown as needed by the realloc above */
-    chunk.size = 0;                   /* no data at this point */
-
-    curl_global_init(CURL_GLOBAL_ALL);
-
-    /* init the curl session */
-    curl_handle = curl_easy_init();
-
-    /* specify URL to get */
-    curl_easy_setopt(curl_handle, CURLOPT_URL, url.str().c_str());
-
-    /* send all data to this function  */
-    curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, WriteMemoryCallback);
-
-    /* we pass our 'chunk' struct to the callback function */
-    curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, (void *)&chunk);
-
-    /* some servers don't like requests that are made without a user-agent
-       field, so we provide one */
-    curl_easy_setopt(curl_handle, CURLOPT_USERAGENT, "libcurl-agent/1.0");
-
-    /* get it! */
-    res = curl_easy_perform(curl_handle);
-
-    /* check for errors */
-    if (res != CURLE_OK)
-    {
-        fprintf(stderr, "curl_easy_perform() failed: %s\n",
-                curl_easy_strerror(res));
-    }
-    else
-    {
-        /*
-         * Now, our chunk.memory points to a memory block that is chunk.size
-         * bytes big and contains the remote file.
-         *
-         * Do something nice with it!
-         */
-
-        ss << chunk.memory;
-
-        printf("%lu bytes retrieved\n", (long)chunk.size);
-    }
-
-    curl_easy_cleanup(curl_handle);
-
-    free(chunk.memory);
-
-    curl_global_cleanup();
-
-    string solution = ss.str();
-
-    // request the server for solution - end --------------------------------
     return solution;
+
+
+
+// // we are not using server because it is too slow
+//     // request the server for solution --------------------------------
+
+//     stringstream ss, url;
+
+//     url << "http://moulinet.tech/sudokus?input=";
+//     url << initialStateOfTheSudoku;
+
+//     CURL *curl_handle;
+//     CURLcode res;
+
+//     struct MemoryStruct chunk;
+
+//     chunk.memory = (char *)malloc(1); /* will be grown as needed by the realloc above */
+//     chunk.size = 0;                   /* no data at this point */
+
+//     curl_global_init(CURL_GLOBAL_ALL);
+
+//     /* init the curl session */
+//     curl_handle = curl_easy_init();
+
+//     /* specify URL to get */
+//     curl_easy_setopt(curl_handle, CURLOPT_URL, url.str().c_str());
+
+//     /* send all data to this function  */
+//     curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, WriteMemoryCallback);
+
+//     /* we pass our 'chunk' struct to the callback function */
+//     curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, (void *)&chunk);
+
+//     /* some servers don't like requests that are made without a user-agent
+//        field, so we provide one */
+//     curl_easy_setopt(curl_handle, CURLOPT_USERAGENT, "libcurl-agent/1.0");
+
+//     /* get it! */
+//     res = curl_easy_perform(curl_handle);
+
+//     /* check for errors */
+//     if (res != CURLE_OK)
+//     {
+//         fprintf(stderr, "curl_easy_perform() failed: %s\n",
+//                 curl_easy_strerror(res));
+//     }
+//     else
+//     {
+//         /*
+//          * Now, our chunk.memory points to a memory block that is chunk.size
+//          * bytes big and contains the remote file.
+//          *
+//          * Do something nice with it!
+//          */
+
+//         ss << chunk.memory;
+
+//         printf("%lu bytes retrieved\n", (long)chunk.size);
+//     }
+
+//     curl_easy_cleanup(curl_handle);
+
+//     free(chunk.memory);
+
+//     curl_global_cleanup();
+
+//     string solution = ss.str();
+
+//     // request the server for solution - end --------------------------------
+    // return solution;
 }
+
 
 string Possible::str(int width) const
 {
